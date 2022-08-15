589a2774b8d142ab3f973537398137852a272fd6
galileoomega <alexis.limacher@outlook.com>
2022-08-15 22:45:18 +0200

    ooko

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,10 +11,10 @@ export class AppComponent {
   title = 'dashboardCI';
 
   public listOfTestsResults: Array<TestResult> = [
-    new TestResult("Fusion FCC Eagle Nighly", "Master", "3866", "28.06.2022  02:06"),
-    new TestResult("Fusion FCC SeaCloud Nightly", "Release/MSM_r2022_1_candidate", "3866", "28.06.2022  02:06"),
-    new TestResult("Fusion FCC FPM Nightly", "Release/MSM_r2022_1_candidate", "3866", "28.06.2022  02:06"),
-    new TestResult("Fusion FCC SeaCloud Daily", "Release/MSM_r2022_1_candidate", "3866", "28.06.2022  02:06")
+    new TestResult("Fusion FCC Eagle Nighly", "Master", "3866", "28.06.2022  02:06", false, "unit test has failed"),
+    new TestResult("Fusion FCC SeaCloud Nightly", "Release/MSM_r2022_1_candidate", "3866", "28.06.2022  02:06", true, ""),
+    new TestResult("Fusion FCC FPM Nightly", "Release/MSM_r2022_1_candidate", "3866", "28.06.2022  02:06", true, ""),
+    new TestResult("Fusion FCC SeaCloud Daily", "Release/MSM_r2022_1_candidate", "3866", "28.06.2022  02:06", true, "")
   ];
 
   public listOfDevicesStatus: Array<DeviceStatus> = [
--- src/app/models/testResult.model.ts
+++ src/app/models/testResult.model.ts
@@ -3,11 +3,15 @@ export class TestResult {
     public planBranch: string;
     public planBuildNumber: string;
     public planDateOfBuild: string;
+    public testValid: boolean;
+    public testResult: string;
 
-    constructor(PlanName: string, PlanBranch: string, PlanBuildNumber: string, PlanDateOfBuild: string) {
+    constructor(PlanName: string, PlanBranch: string, PlanBuildNumber: string, PlanDateOfBuild: string, TestValid: boolean, TestResult: string) {
         this.planName = PlanName;
         this.planBranch = PlanBranch;
         this.planBuildNumber = PlanBuildNumber;
         this.planDateOfBuild = PlanDateOfBuild;
+        this.testValid = TestValid;
+        this.testResult = TestResult;
     }
 }
